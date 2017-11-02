e8d6cbd600391279831364bed3cf7e40ea006c01
redradist <redradist@gmail.com>
2017-11-02 23:27:23 +0200

    Improved test for CommonAPI

--- examples/CommonAPI_Server/main.cpp
+++ examples/CommonAPI_Server/main.cpp
@@ -7,10 +7,11 @@ using namespace std;
 
 int main() {
   HelloWorldStubImpl helloWorldService;
-  helloWorldService.registerService("local", "test");
   while (true) {
+    helloWorldService.registerService("local", "test");
     std::cout << "Waiting for calls... (Abort with CTRL+C)" << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(30));
+    helloWorldService.unregisterService();
   }
   return 0;
 }
