a65c59c5a5e56533efbe72f60bcea097ebae1aeb
redradist <redradist@gmail.com>
2017-11-28 02:10:52 +0200

    Added a new version of commonapi_tools

--- src/commonapi/fidl_generator.cmake
+++ src/commonapi/fidl_generator.cmake
@@ -1,3 +1,6 @@
+execute_process(COMMAND "pip3 install jinja2")
+execute_process(COMMAND "pip3 install regex")
+
 function(add_fidl_dependencies target fidl_files generators_path)
     message(STATUS "target is ${target}")
     message(STATUS "fidl_files is ${fidl_files}")
@@ -10,17 +13,17 @@ function(add_fidl_dependencies target fidl_files generators_path)
         message(STATUS "COMMONAPI_GENERATOR is ${COMMONAPI_GENERATOR}")
         message(STATUS "COMMONAPI_DBUS_GENERATOR is ${COMMONAPI_DBUS_GENERATOR}")
         add_custom_command(OUTPUT ${FIDL_NAME}
-                           DEPENDS ${FIDL}
-                           COMMAND touch ${FIDL_NAME}
-                           COMMAND ${COMMONAPI_GENERATOR} --dest ${generators_path} -sk ${FIDL}
-                           COMMAND ${COMMONAPI_DBUS_GENERATOR} --dest ${generators_path} ${FIDL}
-                          )
+                DEPENDS ${FIDL}
+                COMMAND touch ${FIDL_NAME}
+                COMMAND ${COMMONAPI_GENERATOR} --dest ${generators_path} -sk ${FIDL}
+                COMMAND ${COMMONAPI_DBUS_GENERATOR} --dest ${generators_path} ${FIDL}
+                )
         set(GEN_FIDL_FILES ${GEN_FIDL_FILES} ${FIDL_NAME})
     endforeach()
     message(STATUS "GEN_FIDL_FILES is ${GEN_FIDL_FILES}")
-    add_custom_target(fidl_files_gen
-                      DEPENDS ${GEN_FIDL_FILES}
-                      COMMAND echo "Do it ...")
+    add_custom_target(${target}_fidl_files_gen
+            DEPENDS ${GEN_FIDL_FILES}
+            COMMAND echo "Do it ...")
     foreach(FIDL ${fidl_files})
         message(STATUS "ICC_SOURCE_DIR is ${ICC_SOURCE_DIR}")
         message(STATUS "Started generate CommonAPIWrappers")
@@ -30,20 +33,20 @@ function(add_fidl_dependencies target fidl_files generators_path)
         string(REPLACE ".fidl" "" FIDL_NAME ${FIDL_NAME_WITH_EXTENTION})
         message(STATUS "FIDL_NAME is ${FIDL_NAME}")
         add_custom_command(OUTPUT ${FIDL_NAME}CommonAPIWrappers
-                           DEPENDS fidl_files_gen
-                           COMMAND touch ${FIDL_NAME}CommonAPIWrappers
-                           COMMAND python3 ${ICC_SOURCE_DIR}/src/commonapi/commonapi_tools/commonapi_tools.py
-                                           ${FIDL}
-                                           ${CMAKE_BINARY_DIR}
-                                           --capi_client ${ICC_SOURCE_DIR}/src/commonapi/templates/CommonAPIClient.hpp.jinja2
-                                           --capi_service ${ICC_SOURCE_DIR}/src/commonapi/templates/CommonAPIService.hpp.jinja2
-                          )
+                DEPENDS ${target}_fidl_files_gen
+                COMMAND touch ${FIDL_NAME}CommonAPIWrappers
+                COMMAND python3 ${ICC_SOURCE_DIR}/src/commonapi/commonapi_tools/commonapi_tools.py
+                ${FIDL}
+                ${CMAKE_BINARY_DIR}
+                --capi_client ${ICC_SOURCE_DIR}/src/commonapi/templates/CommonAPIClient.hpp.jinja2
+                --capi_service ${ICC_SOURCE_DIR}/src/commonapi/templates/CommonAPIService.hpp.jinja2
+                )
         set(GEN_FIDL_COMMONAPI_WRAPPER_FILES ${GEN_FIDL_COMMONAPI_WRAPPER_FILES} ${FIDL_NAME}CommonAPIWrappers)
     endforeach()
     message(STATUS "GEN_FIDL_COMMONAPI_WRAPPER_FILES is ${GEN_FIDL_COMMONAPI_WRAPPER_FILES}")
-    add_custom_target(fidl_commonapi_wrappers_gen
-                      DEPENDS ${GEN_FIDL_COMMONAPI_WRAPPER_FILES}
-                      COMMAND echo "Do it ...")
-    add_dependencies(${target} fidl_files_gen fidl_commonapi_wrappers_gen)
+    add_custom_target(${target}_fidl_commonapi_wrappers_gen
+            DEPENDS ${GEN_FIDL_COMMONAPI_WRAPPER_FILES}
+            COMMAND echo "Do it ...")
+    add_dependencies(${target} ${target}_fidl_files_gen ${target}_fidl_commonapi_wrappers_gen)
 
 endfunction(add_fidl_dependencies)
