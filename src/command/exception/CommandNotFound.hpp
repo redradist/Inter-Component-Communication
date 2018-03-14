a2df7115896504fa9afda79341abf8201a0a8059
redradist <redradist@gmail.com>
2018-03-15 01:53:09 +0200

    Small fixes

--- src/command/exception/CommandNotFound.hpp
+++ src/command/exception/CommandNotFound.hpp
@@ -1,6 +1,10 @@
-//
-// Created by redra on 15.03.18.
-//
+/**
+ * @file CommandNotFound.hpp
+ * @author Denis Kotov
+ * @date 15 Mar 2018
+ * @brief Contains exception for Not Found Command for ICC library
+ * @copyright Denis Kotov, MIT License. Open source: https://github.com/redradist/Inter-Component-Communication.git
+ */
 
 #ifndef COMMONAPI_CLIENT_COMMANDNOTFOUND_HPP
 #define COMMONAPI_CLIENT_COMMANDNOTFOUND_HPP
--- src/exceptions/ICCException.hpp
+++ src/exceptions/ICCException.hpp
@@ -1,6 +1,10 @@
-//
-// Created by redra on 15.03.18.
-//
+/**
+ * @file ICCException.hpp
+ * @author Denis Kotov
+ * @date 15 Mar 2018
+ * @brief Contains base class exception for ICC library
+ * @copyright Denis Kotov, MIT License. Open source: https://github.com/redradist/Inter-Component-Communication.git
+ */
 
 #ifndef COMMONAPI_CLIENT_ICCEXCEPTION_HPP
 #define COMMONAPI_CLIENT_ICCEXCEPTION_HPP
