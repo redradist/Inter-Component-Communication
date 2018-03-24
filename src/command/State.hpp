2a3bd01dc887bf967c1862cb730634ac85952f40
redradist <redradist@gmail.com>
2018-03-24 23:04:58 +0200

    Added comments for State and LoopMode

--- src/command/State.hpp
+++ src/command/State.hpp
@@ -1,6 +1,10 @@
-//
-// Created by redra on 24.03.18.
-//
+/**
+ * @file State.hpp
+ * @author Denis Kotov
+ * @date 24 Mar 2018
+ * @brief Contains Command State
+ * @copyright Denis Kotov, MIT License. Open source: https://github.com/redradist/Inter-Component-Communication.git
+ */
 
 #ifndef COMMONAPI_CLIENT_COMMANDSTATE_HPP
 #define COMMONAPI_CLIENT_COMMANDSTATE_HPP
