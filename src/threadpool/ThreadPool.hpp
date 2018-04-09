3f54dcb8ca6826a1edc86b97d4ee8e29e80d9eec
redradist <redradist@gmail.com>
2018-04-09 21:17:16 +0300

    Added ThreadPool support in library

---
+++ src/threadpool/ThreadPool.hpp
@@ -0,0 +1,63 @@
+/**
+ * @file ThreadPool.hpp
+ * @author Denis Kotov
+ * @date 08 Apr 2018
+ * @brief Throws when task is invalid
+ * @copyright Denis Kotov, MIT License. Open source: https://github.com/redradist/Inter-Component-Communication.git
+ */
+
+#ifndef ICC_THREADPOOL_HPP
+#define ICC_THREADPOOL_HPP
+
+#include <IComponent.hpp>
+#include <boost/asio/io_service.hpp>
+#include <queue>
+#include <mutex>
+
+namespace icc {
+
+namespace pools {
+
+class ThreadPool {
+  struct IoServiceMetaData {
+    std::shared_ptr<boost::asio::io_service::work> worker_;
+    size_t number_of_tasks_ = 0;
+
+    IoServiceMetaData(std::shared_ptr<boost::asio::io_service> _service) {
+      worker_ = std::make_shared<boost::asio::io_service::work>(*_service);
+    }
+  };
+
+  struct IoServicesComparator {
+    bool operator()(const IoServiceMetaData& _first,
+                    const IoServiceMetaData& _second) const {
+      return _first.number_of_tasks_ > _second.number_of_tasks_;
+    }
+  };
+
+ public:
+  ThreadPool();
+  ~ThreadPool();
+
+  static ThreadPool & getPool();
+
+  /**
+   * Method used to push task for execution
+   * @param _task Task that will be executed
+   */
+  void push(std::function<void(void)> _task);
+
+ protected:
+  std::recursive_mutex mutex_;
+  std::vector<std::thread> thread_pool_;
+  std::vector<std::shared_ptr<boost::asio::io_service>> services_;
+  std::priority_queue<IoServiceMetaData,
+                      std::vector<IoServiceMetaData>,
+                      IoServicesComparator> services_meta_data_;
+};
+
+}
+
+}
+
+#endif //ICC_THREADPOOL_HPP
